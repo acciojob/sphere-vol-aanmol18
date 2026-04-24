5be49ef98be0516febc449559d94cd8ab3a101d1
aanmol18 <anmoly683@gmail.com>
2026-04-24 14:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-	const r = document.querySelector("#radius");
-	const v = document.querySelector("#volume");
-	let cal = (4*3.14*Number.(r)*Number(r)*Number(r))/3;
-    v.innerHTML = "cal"
+	e.preventDefault();
+	const r = Number(document.getElementById("radius").value);
+
+    let volume;
+    if (isNaN(r) || r < 0) {
+        volume = NaN;
+    } else {
+        volume = (4/3) * Math.PI * Math.pow(r, 3);
+        volume = volume.toFixed(4); // round to 4 decimal places
+    }
+
+    document.getElementById("volume").value = volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
