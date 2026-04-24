fb62b7a51e295a45758020ccf4e37d9146f1fbce
aanmol18 <anmoly683@gmail.com>
2026-04-24 13:44:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
     //Write your code here
-  
+	const r = document.querySelector("#radius");
+	const v = document.querySelector("#volume");
+	let cal = (4*3.14*Number.(r)*Number(r)*Number(r))/3;
+    v.innerHTML = "cal"
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
